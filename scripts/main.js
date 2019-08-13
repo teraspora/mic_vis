62e25c618b92c80d1445b1c3d511b990d7c4fb80
John Lynch <johnlynch.32768@gmail.com>
2019-08-13 21:43:17 +0100

    Add distortion and handler to make distortion level dependent on mouse click x position; remove player; tidy up code.

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,8 @@ canvas = document.getElementById('canvas');
 [WIDTH, HEIGHT] = [canvas.clientWidth, canvas.clientHeight];
 HALF_HEIGHT = HEIGHT / 2;
 ctx = canvas.getContext('2d');
+canvas.addEventListener('click', clickHandler);
     
-
 document.addEventListener('click', function() {
     context.resume().then(() => {
         console.log('Playback resumed successfully');
@@ -23,20 +23,13 @@ document.addEventListener('click', function() {
 const constraints = {audio: true};
 navigator.getUserMedia(constraints, processStream, err => console.log(`Error: ${err.name} - ${err.message}`));
 
-
 // ----------------------------------------------------------------------------------------------------------
-// let audio = new Audio();
-// audio.src = 'track1.mp3';
-// audio.controls = true;
-// audio.loop = true;
-// audio.autoplay = true;
-let frame = 0;
-let mic;
-const bar_width = 4;
-const bars = 256;
 
+let frame = 0;
+let mic, dist;
 
 // window.addEventListener("load", initPlayer, false);
+
 function processStream(stream) {
     console.log(stream);
     context = new AudioContext();
@@ -44,13 +37,12 @@ function processStream(stream) {
     analyser = context.createAnalyser();
     mic.connect(analyser);
     // mic.connect(context.destination);
+    dist = context.createWaveShaper();
+    mic.connect(dist);
+    dist.connect(context.destination);
+    dist.curve = makeDistortionCurve(-2.4);
     paintGraph();    
 }
-
-// function initPlayer(){
-//     document.getElementById('audio_box').appendChild(audio);
-//     // Re-route audio playback into the processing graph of the AudioContext
-// }
 
 function paintGraph(){
     requestAnimationFrame(paintGraph);
@@ -66,7 +58,7 @@ function paintGraph(){
     for (let i = 0; i < count; i++, x += sliceWidth) {
         let v = spectrum[i];
         // if (frame % 255 == i) console.log(`v = ${v}`);
-        let y = v  / 128.0 * HALF_HEIGHT - 184;
+        let y = v  / 128 * HALF_HEIGHT - 184;
         ctx.strokeStyle = `rgb(255, 0, ${10 * v - 1152})`;
         ctx.lineWidth = `1`;
         if (i === 0) {
@@ -78,6 +70,43 @@ function paintGraph(){
     }
     ctx.lineTo(WIDTH, HALF_HEIGHT);
     ctx.stroke();
+}
+
+// from https://codepen.io/gregh/pen/OWrjOb:
+// http://stackoverflow.com/a/22313408/1090298
+function makeDistortionCurve( amount ) {
+    let n_samples = 256, curve = new Float32Array(n_samples);
+    for (let i = 0 ; i < n_samples; ++i ) {
+        let x = i * 2 / n_samples - 1;
+        curve[i] = (Math.PI + amount) * x / (Math.PI + amount * Math.abs(x));
+    }
+    return curve;
+} 
+//   let k = typeof amount === 'number' ? amount : 0,
+//     n_samples = 44100,
+//     curve = new Float32Array(n_samples),
+//     deg = Math.PI / 180,
+//     i = 0,
+//     x;
+//   for ( ; i < n_samples; ++i ) {
+//     x = i * 2 / n_samples - 1;
+//     curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+//   }
+//   return curve;
+// }
+
+function getMousePos(c, e) {       // got from https://codepen.io/chrisjaime/pen/lcEpn; takes a canvas and an event (mouse-click)
+    const bounds = c.getBoundingClientRect();
+    return {
+        x: e.clientX - bounds.left,
+        y: e.clientY - bounds.top
+    };
+}
+
+function clickHandler(ev) {
+    const p = getMousePos(canvas, ev).x * 200 / WIDTH - 100;
+    dist.curve = makeDistortionCurve(p)
+    console.log(p);
 }
 
 // function rgb2Hex(rgbColour) {
